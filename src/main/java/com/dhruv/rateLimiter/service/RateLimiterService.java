68e9e6bd87394b10552812e385a6e7f8d3bcf7b3
Dhruv Prajapati <dhruv.prajapati1772@gmail.com>
2023-08-03 13:06:50 +0530

    Implemented GlobalUser andGlobalApi functionality

--- src/main/java/com/dhruv/rateLimiter/service/RateLimiterService.java
+++ src/main/java/com/dhruv/rateLimiter/service/RateLimiterService.java
@@ -5,14 +5,19 @@ import com.dhruv.rateLimiter.repository.RateLimiterRepository;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @Service
 public class RateLimiterService {
     private final RateLimiterRepository rateLimiterRepository;
-    private final int API_RATE_LIMIT = 10; // Number of API calls per minute
-    private final int GLOBAL_USER_RATE_LIMIT = 6; // Total number of API calls per minute allowed for a user
-    private final int GLOBAL_API_RATE_LIMIT = 4; // Total number of API calls per minute allowed for any API
-
+    private final int API_RATE_LIMIT = 7; // Number of API calls per minute
+    private final int GLOBAL_USER_RATE_LIMIT = 4; // Total number of API calls per minute allowed for a user
+    private final int GLOBAL_API_RATE_LIMIT = 3; // Total number of API calls per minute allowed for any API
+    private static final Logger logger = Logger.getLogger(RateLimiterService.class.getName());
+    private static RateLimiterUser apiUser;
+    private static RateLimiterUser globalUser;
+    private static RateLimiterUser globalApiUser;
     public RateLimiterService(RateLimiterRepository rateLimiterRepository) {
         this.rateLimiterRepository = rateLimiterRepository;
     }
@@ -27,96 +32,98 @@ public class RateLimiterService {
         if (!globalUserRateLimiter(userId)) {
             return false;
         }
-
+        
         // Perform Global API Rate Limiter check
         if (!globalApiRateLimiter(apiName)) {
             return false;
         }
-
+        rateLimiterRepository.save(apiUser);
+        rateLimiterRepository.save(globalUser);
+        rateLimiterRepository.save(globalApiUser);
         return true;
     }
 
     private boolean apiSpecificRateLimiter(Long userId, String apiName) {
-        RateLimiterUser userEntry = rateLimiterRepository.findByUserIdAndApiName(userId, apiName);
+        apiUser = rateLimiterRepository.findByUserIdAndApiName(userId, apiName);
         Instant now = Instant.now();
-        Instant lastRequestTime = (userEntry != null) ? userEntry.getLastRequestTime() : now.minusSeconds(60);
+        Instant lastRequestTime = (apiUser != null) ? apiUser.getLastRequestTime() : now.minusSeconds(60);
         if (now.isAfter(lastRequestTime.plusSeconds(60)) || 
             now.equals(lastRequestTime.plusSeconds(60))){
             
             // Reset request count if the interval has passed
-            if (userEntry == null) {
-                userEntry = new RateLimiterUser();
-                userEntry.setUserId(userId);
-                userEntry.setApiName(apiName);
+            if (apiUser == null) {
+                apiUser = new RateLimiterUser();
+                apiUser.setUserId(userId);
+                apiUser.setApiName(apiName);
             }
-            userEntry.setLastRequestTime(now);
-            userEntry.setRequestCount(1);
+            apiUser.setLastRequestTime(now);
+            apiUser.setRequestCount(1);
         }
         else {
-            int requestCount = userEntry.getRequestCount() + 1;
-            if (requestCount > API_RATE_LIMIT) {
+            int requestCount = apiUser.getRequestCount();
+            if (requestCount+1 > API_RATE_LIMIT) {
                 return false; // Limit exceeded
             }
-            userEntry.setRequestCount(requestCount);
+            apiUser.setRequestCount(requestCount+1);
         }
 
-        rateLimiterRepository.save(userEntry);
+        logger.log(Level.INFO, "apiSpecifiRateLimiter passed!");
         return true;
     }
 
     private boolean globalUserRateLimiter(Long userId) {
-        RateLimiterUser userEntry = rateLimiterRepository.findByUserIdAndApiName(userId, "global_user");
+        globalUser = rateLimiterRepository.findByUserIdAndApiName(userId, "global_user");
         Instant now = Instant.now();
-        Instant lastRequestTime = (userEntry != null) ? userEntry.getLastRequestTime() : now.minusSeconds(60);
+        Instant lastRequestTime = (globalUser != null) ? globalUser.getLastRequestTime() : now.minusSeconds(60);
 
         if (now.isAfter(lastRequestTime.plusSeconds(60)) || 
             now.equals(lastRequestTime.plusSeconds(60))) {
             
             // Reset request count if the interval has passed
-            if (userEntry == null) {
-                userEntry = new RateLimiterUser();
-                userEntry.setUserId(userId);
-                userEntry.setApiName("global_user");
+            if (globalUser == null) {
+                globalUser = new RateLimiterUser();
+                globalUser.setUserId(userId);
+                globalUser.setApiName("global_user");
             }
-            userEntry.setLastRequestTime(now);
-            userEntry.setRequestCount(1);
+            globalUser.setLastRequestTime(now);
+            globalUser.setRequestCount(1);
         } else {
-            int requestCount = userEntry.getRequestCount() + 1;
-            if (requestCount > GLOBAL_USER_RATE_LIMIT) {
+            int requestCount = globalUser.getRequestCount();
+            if (requestCount+1 > GLOBAL_USER_RATE_LIMIT) {
                 return false; // Limit exceeded
             }
-            userEntry.setRequestCount(requestCount);
+            globalUser.setRequestCount(requestCount+1);
         }
-
-        rateLimiterRepository.save(userEntry);
+        
+        logger.log(Level.INFO, "globalUserRateLimiter passed!");
         return true;
     }
 
     private boolean globalApiRateLimiter(String apiName) {
-        RateLimiterUser userEntry = rateLimiterRepository.findByUserIdAndApiName(-1L, apiName);
+        globalApiUser = rateLimiterRepository.findByUserIdAndApiName(-1L, apiName);
         Instant now = Instant.now();
-        Instant lastRequestTime = userEntry != null ? userEntry.getLastRequestTime() : now.minusSeconds(60);
+        Instant lastRequestTime = globalApiUser != null ? globalApiUser.getLastRequestTime() : now.minusSeconds(60);
 
         if (now.isAfter(lastRequestTime.plusSeconds(60))|| 
             now.equals(lastRequestTime.plusSeconds(60))){
             
             // Reset request count if the interval has passed
-            if (userEntry == null) {
-                userEntry = new RateLimiterUser();
-                userEntry.setUserId(-1L);
-                userEntry.setApiName(apiName);
+            if (globalApiUser == null) {
+                globalApiUser = new RateLimiterUser();
+                globalApiUser.setUserId(-1L);
+                globalApiUser.setApiName(apiName);
             }
-            userEntry.setLastRequestTime(now);
-            userEntry.setRequestCount(1);
+            globalApiUser.setLastRequestTime(now);
+            globalApiUser.setRequestCount(1);
         } else {
-            int requestCount = userEntry.getRequestCount() + 1;
-            if (requestCount > GLOBAL_API_RATE_LIMIT) {
+            int requestCount = globalApiUser.getRequestCount();
+            if (requestCount+1 > GLOBAL_API_RATE_LIMIT) {
                 return false; // Limit exceeded
             }
-            userEntry.setRequestCount(requestCount);
+            globalApiUser.setRequestCount(requestCount+1);
         }
-
-        rateLimiterRepository.save(userEntry);
+        
+        logger.log(Level.INFO, "globalApiRateLimiter passed!");
         return true;
     }
 }
